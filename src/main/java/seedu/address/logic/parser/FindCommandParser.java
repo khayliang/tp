8e85ae6a1fa7f53de24541ae3cd1d02d55bb87d5
Tan Yu Quan <22824928+yuquanzzz@users.noreply.github.com>
2026-03-10 05:23:19 +0800

    Refactor FindCommandParser to support subcommand parsing

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,33 +1,55 @@
 package seedu.address.logic.parser;
 
+import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.FindPersonCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
- * Parses input arguments and creates a new FindCommand object
+ * Parses input arguments for find and dispatches to the relevant find subcommand parser.
  */
 public class FindCommandParser implements Parser<FindCommand> {
 
+    private final Map<String, Parser<? extends FindCommand>> subCommandParsers;
+
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * Constructs a FindCommandParser and initializes the subcommand parsers.
+     */
+    public FindCommandParser() {
+        Map<String, Parser<? extends FindCommand>> parsers = new HashMap<>();
+        parsers.put(FindPersonCommand.SUB_COMMAND_WORD, new FindPersonCommandParser());
+        this.subCommandParsers = Collections.unmodifiableMap(parsers);
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of find
+     * and returns a concrete find subcommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        String[] splitArgs = trimmedArgs.split("\\s+", 2);
+        String subcommand = splitArgs[0];
+        String subcommandArgs = splitArgs.length > 1 ? splitArgs[1] : "";
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        Parser<? extends FindCommand> subCommandParser = subCommandParsers.get(subcommand);
+        if (subCommandParser == null) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+        return subCommandParser.parse(subcommandArgs);
     }
 
 }
